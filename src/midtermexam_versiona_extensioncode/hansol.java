ae7188d89828b7503a42c01d4b2e3751492e96ac
Hansol Na <nahans.sheridancollege.ca>
2023-06-23 13:22:02 -0400

    I am commiting my newly created Branch now. Friday June 23, 2023 @ 1:21pm

---
+++ src/midtermexam_versiona_extensioncode/hansol.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Hansol
+ */
+//this is the new hansol class for the Midterm.
+//created on Friday Jun 23, 2023 @ 1:20pm
+public class hansol {
+    //For the midterm.
+}
